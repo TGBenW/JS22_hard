14d2910cf0b25d1b17274d12d9e0a88ae3704075
TGBenW <dukariga@gmail.com>
2021-12-04 05:38:53 +0200

    lesson05_2
    prime numbers, array

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
+const arrLength = 7;
+
+let arr = [];
+
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-const getAllServicePrices = function () {
-  let sum = 0;
-
+const promptNumber = function () {
   let n;
   do {
     n = prompt("Введите число");
@@ -14,4 +16,42 @@ const getAllServicePrices = function () {
   return +n;
 };
 
-console.log(getAllServicePrices());
+const fillArray = function () {
+  for (let i = 0; i < arrLength; i++) {
+    arr[i] = promptNumber();
+  }
+};
+
+const checkArray = function () {
+  for (let i = 0; i < arrLength; i++) {
+    if (arr[i].toString()[0] == 2 || arr[i].toString()[0] == 4) {
+      console.log(arr[i]);
+    }
+  }
+};
+
+const isPrime = function (prime) {
+  let counter = 0;
+  for (let i = 2; i <= prime; i++) {
+    if (prime % i == 0) {
+      counter++;
+    }
+  }
+  if (counter == 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const primeNumbers = function () {
+  for (let i = 2; i <= 100; i++) {
+    if (isPrime(i)) {
+      console.log(i + " - делители этого числа: 1 и " + i);
+    }
+  }
+};
+
+fillArray();
+checkArray();
+primeNumbers();
