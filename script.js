bff00c7aaccdf4463056d28386b9c5688246d6c8
TGBenW <dukariga@gmail.com>
2021-12-04 04:47:49 +0200

    lesson05_1
    isNumber spaces check

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 "use strict";
 
-let string = prompt("Enter your string please");
-
-const stringManage = function (str) {
-  if (typeof str !== "string") {
-    return "Please enter a string";
-  } else {
-    string = string.trim();
-    if (string.length > 30) {
-      return string.substr(0, 30) + "...";
-    } else {
-      return string;
-    }
-  }
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-console.log(stringManage(string));
+const getAllServicePrices = function () {
+  let sum = 0;
+
+  let n;
+  do {
+    n = prompt("Введите число");
+  } while (!isNumber(n));
+  return +n;
+};
+
+console.log(getAllServicePrices());
